255f402135660aea9ef69d33ef6c0e2201c75e9e
Juan López <juan@example.com>
2026-03-08 15:08:05 -0600

    Changes in region

--- tests/test_dynamoConection.py
+++ tests/test_dynamoConection.py
@@ -3,7 +3,7 @@ import boto3
 # connect to DynamoDB
 dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
 
-table = dynamodb.Table("products")
+table = dynamodb.Table("orders")
 
 # test read
 response = table.scan(Limit=1)
